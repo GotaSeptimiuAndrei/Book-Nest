1e7884905a0f539ee03253cc149621f6660c59ea
Goța Septimiu Andrei <andreigota2003@gmail.com>
2024-09-15 19:27:30 +0300

    add 'find by title' functionality

--- backend/src/main/java/com/backend/dao/BookRepository.java
+++ backend/src/main/java/com/backend/dao/BookRepository.java
@@ -2,7 +2,10 @@ package com.backend.dao;
 
 import com.backend.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
-
+    Page<Book> findByTitleContaining(@RequestParam("title") String title, Pageable pageable);
 }
