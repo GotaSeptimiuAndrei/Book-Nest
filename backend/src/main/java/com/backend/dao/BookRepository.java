82813b6d8905ea63d8288096ec466db6aaf810c5
Goța Septimiu Andrei <andreigota2003@gmail.com>
2024-09-15 19:40:01 +0300

    add 'find by category' functionality

--- backend/src/main/java/com/backend/dao/BookRepository.java
+++ backend/src/main/java/com/backend/dao/BookRepository.java
@@ -8,4 +8,5 @@ import org.springframework.data.domain.Pageable;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
     Page<Book> findByTitleContaining(@RequestParam("title") String title, Pageable pageable);
+    Page<Book> findByCategory(@RequestParam("category") String category, Pageable pageable);
 }
