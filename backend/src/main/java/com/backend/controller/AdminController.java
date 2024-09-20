63b89a98ea8ed091e9c41be00b7be50e297ecb3e
Goța Septimiu Andrei <andreigota2003@gmail.com>
2024-09-20 16:08:34 +0300

    'admin adds book' functionality

---
+++ backend/src/main/java/com/backend/controller/AdminController.java
@@ -0,0 +1,29 @@
+package com.backend.controller;
+
+import com.backend.requestModels.AddBookRequest;
+import com.backend.service.AdminService;
+import com.backend.utils.ExtractJWT;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api/admin")
+public class AdminController {
+    private AdminService adminService;
+
+    @Autowired
+    public AdminController(AdminService adminService) {
+        this.adminService = adminService;
+    }
+
+    @PostMapping("/secure/add/book")
+    public void postBook(@RequestHeader(value="Authorization") String token,
+                         @RequestBody AddBookRequest addBookRequest) throws Exception {
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+        if (admin == null || !admin.equals("admin")) {
+            throw new Exception("Administrator access only");
+        }
+        adminService.postBook(addBookRequest);
+    }
+}
