d9d3a094b8facb32a2acedcf87dd3d62bf427ca8
Goța Septimiu Andrei <andreigota2003@gmail.com>
2024-10-14 17:22:48 +0300

    update redirectUri in oktaConfig.ts

--- frontend/src/lib/oktaConfig.ts
+++ frontend/src/lib/oktaConfig.ts
@@ -1,7 +1,7 @@
 export const oktaConfig = {
   clientId: "0oajrtourvx6WZWDF5d7",
   issuer: "https://dev-06465044.okta.com/oauth2/default",
-  redirectUri: "http://localhost:3000/login/callback",
+  redirectUri: "https://booknestlibrary.netlify.app/login/callback",
   scopes: ["openid", "profile", "email"],
   pkce: true,
   disableHttpsCheck: true
